04f1f54ed198d8f69a6144f2287d3c4819758729
Niels Lemmens <draso.odin@gmail.com>
2016-04-13 09:52:15 +0200

    Fix bash

--- update-version.sh
+++ update-version.sh
@@ -6,7 +6,8 @@ echo FROM python:3.5 > Dockerfile
 echo RUN pip install -U pip >> Dockerfile
 echo RUN pip install $(pip freeze | grep libsass) >> Dockerfile
 
-VERSION = $(pip freeze | grep libsass | sed 's/.*==//')
+VERSION=$(pip freeze | grep libsass | sed 's/.*==//')
+
 git commit Dockerfile -m "Update libsass to $VERSION"
 
 git tag -a $VERSION -m "Update to version $VERSION"
